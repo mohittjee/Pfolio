f6bbeb09cdbb50591b6096a21d5f0b626055fed4
Mohit Kumar <mohittjee@gmail.com>
2024-06-30 12:17:15 +0530

    enabled log feature

--- src/app/blog/(posts)/layout.tsx
+++ src/app/blog/(posts)/layout.tsx
@@ -1,17 +1,52 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useEffect, useState, useRef } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<main className="overflow-hidden">
-			<menu className="flex justify-end py-4">
-				<Button asChild>
-					<Link href={"/blog"}>Return to blog</Link>
-				</Button>
-			</menu>
-			<section className="prose">{children}</section>
-		</main>
-	);
+  const [isSticky, setIsSticky] = useState(false);
+  const buttonRef = useRef<HTMLMenuElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (buttonRef.current) {
+        const offsetTop = buttonRef.current.getBoundingClientRect().top + window.scrollY;
+        setIsSticky(window.scrollY > offsetTop);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const stickyStyles = isSticky
+    ? {
+        position: "fixed",
+        top: 0,
+        width: buttonRef.current?.offsetWidth, // Ensure the width matches its original width
+        zIndex: 1000,
+        padding: "1rem 0 .5rem 0 ",
+      }
+    : {};
+
+  return (
+    <main className="overflow-hidden">
+      <div ref={buttonRef} style={{ position: "relative" }}>
+        <menu
+          className="flex justify-end py-4"
+          style={stickyStyles}
+        >
+          <Button asChild className="rounded-full border opacity-85 border-gray-300 shadow-lg ">
+            <Link href={"/blog"}>Return to blog</Link>
+          </Button>
+        </menu>
+      </div>
+      <section className="prose">{children}</section>
+    </main>
+  );
 };
 
 export default Layout;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Header from "@/components/Header";
 import {
 	Card,
@@ -7,26 +8,29 @@ import {
 } from "@/components/ui/card";
 import { blogPosts } from "@/lib/utils";
 import Link from "next/link";
+import { Separator } from "../../components/ui/separator";
 
 function Page() {
 	return (
-		<div className="grid grid-cols-1 gap-4">
-			<Header>
-				<h2 className="font-medium text-2xl tracking-wide text-muted-foreground">
-					blog
-				</h2>
-			</Header>
+		// <div className="relative grid grid-cols-1 ">
+		<div className="fixed inset-0 -z-30 dark:bg-gradient-to-tr from-background from-20% via-[#3f6576] via-50% to-background to-80% h-full w-full" >
 
-			{blogPosts.reverse().map((post, i) => (
-				<Link key={i} href={`/blog/${post.href}`}>
-					<Card>
-						<CardHeader>
-							<CardTitle>{post.title}</CardTitle>
-							<CardDescription>{post.date}</CardDescription>
-						</CardHeader>
-					</Card>
-				</Link>
-			))}
+			<div className=" grid grid-flow-row gap-5 ">
+				
+				<span className="pt-2 font-medium text-center text-3xl tracking-wide text-muted-foreground dark:text-[#8E8FFA]">Blogs</span>
+				<Separator/>
+
+				{blogPosts.reverse().map((post, i) => (
+					<Link key={i} href={`/blog/${post.href}`}>
+						<Card className="shadow-xl ml-60 rounded-2xl dark:bg-card dark:hover:bg-zinc-900 cursor-pointer w-3/6">
+							<CardHeader>
+								<CardTitle>{post.title}</CardTitle>
+								<CardDescription>{post.date}</CardDescription>
+							</CardHeader>
+						</Card>
+					</Link>
+				))}
+			</div>
 		</div>
 	);
 }
