260a6de0f94c1a4a919bca81453b04239b905e40
Mohit Kumar <mohittjee@gmail.com>
2024-07-02 11:08:00 +0530

    z index fixed

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -13,16 +13,16 @@ import { Separator } from "../../components/ui/separator";
 function Page() {
 	return (
 		// <div className="relative grid grid-cols-1 ">
-		<div className="fixed inset-0 -z-30 dark:bg-gradient-to-tr from-background from-20% via-[#3f6576] via-50% to-background to-80% h-full w-full" >
+		<div className="fixed inset-0 z-1  dark:bg-gradient-to-tr from-background from-20% via-[#3f6576] via-50% to-background to-80% h-full w-full" >
 
-			<div className=" grid grid-flow-row gap-5 ">
+			<div className=" grid grid-cols-1 gap-5 ">
 				
 				<span className="pt-2 font-medium text-center text-3xl tracking-wide text-muted-foreground dark:text-[#8E8FFA]">Blogs</span>
 				<Separator/>
 
 				{blogPosts.reverse().map((post, i) => (
 					<Link key={i} href={`/blog/${post.href}`}>
-						<Card className="shadow-xl ml-60 rounded-2xl dark:bg-card dark:hover:bg-zinc-900 cursor-pointer w-3/6">
+						<Card className="shadow-xl max-w-90 ml-28 mr-28 rounded-2xl bg-card dark:bg-card dark:hover:bg-zinc-900 cursor-pointer">
 							<CardHeader>
 								<CardTitle>{post.title}</CardTitle>
 								<CardDescription>{post.date}</CardDescription>
