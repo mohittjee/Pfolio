531d54a7ecfd756710956fbab0001fbe1c195567
Mohit Kumar <mohittjee@gmail.com>
2024-07-02 11:08:59 +0530

    button sixe fixed still need fixation for small screen

--- src/components/calcom.tsx
+++ src/components/calcom.tsx
@@ -2,12 +2,13 @@
 import { getCalApi } from "@calcom/embed-react";
 import { useEffect } from "react";
 import {motion} from "framer-motion";
+import { SizeIcon } from "@radix-ui/react-icons";
 
 export default function Calcom(){
 	useEffect(()=>{
 	  (async function () {
 		const cal = await getCalApi({});
-		cal("floatingButton", {"calLink":"offsense/15min","buttonText":"Book my Cal","hideButtonIcon":false,"buttonColor":"#ffffff","buttonTextColor":"#000000" });
+		cal("floatingButton", {"calLink":"offsense/15min","buttonText":"Book my Cal","hideButtonIcon":false,"buttonColor":"#ffffff","buttonTextColor":"#000000", "buttonPosition":"bottom-right", });
 		cal("ui", {"theme":"auto","styles":{"branding":{"brandColor":"#E32A0C"}},"hideEventTypeDetails":false,"layout":"month_view"});
 	  })();
 	}, [])
